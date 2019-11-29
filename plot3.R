f6b3e99a5eaa25db511f6fd4f5ec2f1019b882ee
Bertrand Debrosise <Bertrand_Debroise@Yahoo.fr>
2019-11-29 17:22:31 +0100

    commit for plot3

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 #
 # EXPLORATORY DATA ANALYSIS
 #
-# PLOT 2 : GLOBAL ACTIVE POWER
+# PLOT 3 : GLOBAL ACTIVE POWER
 #
 
 #  Project_folder (UCI_EPC)     -> working directory      
@@ -56,10 +56,17 @@ tbl_epc <- tbl_epc %>% mutate(Global_active_power=as.numeric(Global_active_power
 
 
 
-# PLOT
-png("Plot2.png", width = 480, height = 480)
 
-plot(tbl_epc$fulldatetime,tbl_epc$Global_active_power,type="l",ylab = "Global Active Power (kilowatts)",xlab="")
+# PLOT
+png("Plot3.png", width = 480, height = 480)
+
+plot(tbl_epc$fulldatetime,tbl_epc$Sub_metering_1,type="l",ylab = "Energy sub metering",xlab="")
+
+points(tbl_epc$fulldatetime,tbl_epc$Sub_metering_2,type="l",col="red")
+
+points(tbl_epc$fulldatetime,tbl_epc$Sub_metering_3,type="l",col="blue")
+
+legend("topright", lty =1 , col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
 
 dev.off()  ## Don't forget to close the PNG device!
 
